cc23e20d651cfb8dc16c18dba9f789a2ab208184
Trent McNabb <trentmcnabb@hotmail.com>
2021-10-18 20:37:32 -0400

    decent model

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    return 
+def index():
+    title = 'title'
+    return render_template('index.html',title=title)
 
 
